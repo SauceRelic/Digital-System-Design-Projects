25bd81148d2ba172ea57979c363c7c8ae78319ce
SauceRelic <39061831+SauceRelic@users.noreply.github.com>
2018-09-06 11:49:10 -0500

    final

---
+++ Lab 1/Counter_updn_n_bit_modif.vhdl
@@ -0,0 +1,74 @@
+--------------------------------------
+--
+-- Counter_updn_n_bit_modif.vhdl
+--
+-- created 9/6/18
+-- Phillip Hiemenz
+-- Modified Counter_updn_n_bit.vhdl by tj
+--
+-- rev 0
+----------------------------------------
+--
+-- n bit up/down counter example
+-- for showing a project from start to finish
+--
+----------------------------------------
+--
+-- Inputs: rstb, clk, dir
+-- Outputs: bout[n-1:0]
+--
+----------------------------------------
+--
+-- counts up when dir = 0
+-- counts down when dir = 1
+--
+----------------------------------------
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity Counter_updn_n_bit_modif is
+  generic(
+    n: natural := 8
+  );
+  port (
+    clk: in std_logic;
+    rstb: in std_logic;
+    dir: in std_logic;
+    bout : out std_logic_vector(n-1 downto 0)
+  );
+end entity;
+
+architecture behavioral of Counter_updn_n_bit_modif is
+--
+-- internal signals
+--
+  signal bout_sig: unsigned(n-1 downto 0);
+
+begin
+  process(clk, rstb)
+  begin
+--
+-- reset
+--
+    if (rstb = '0') then
+      bout_sig <= (others => '0');
+--
+-- rising clk edge
+--
+    elsif (rising_edge (clk)) then
+      if(dir = '0') then
+        bout_sig <= bout_sig + 2;
+      else
+        bout_sig <= bout_sig - 3;
+      end if;
+    end if;
+  end process;
+--
+-- Output logic
+--
+  bout <= std_logic_vector(bout_sig);
+end behavioral;
+
---
+++ Lab 1/Counter_updn_n_bit_modif_de10.vhdl
@@ -0,0 +1,66 @@
+------------------------------
+--
+-- counter_updn_n_bit_modif_de10.vhdl
+--
+-- created: 3/17/18
+-- by: johnsontimoj
+-- rev: 0
+--
+-- DE10 implementation for up down counter
+-- of Counter_updn_n_bit_modif.vhdl
+--
+-- Uses a 3 Hz clock divider, sw0 for reset
+-- sw1 for the dir input
+-- and LEDs for the count output
+-------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity counter_updn_n_bit_modif_de10 is
+  port(
+    CLOCK_50: in std_logic;
+    SW: in std_logic_vector(1 downto 0);
+    LEDR: out std_logic_vector(7 downto 0)
+  );
+end entity;
+
+architecture hardware of counter_updn_n_bit_modif_de10 is
+  signal CLK_SIG: std_logic; -- Intermediate clk signal
+--------------------------------
+-- Component prototypes
+---------------------------------
+  component clk_1Hz
+    port(
+      clk_50MHz : IN STD_LOGIC;
+      rstb : IN STD_LOGIC;
+      clk_1Hz : OUT STD_LOGIC
+    );
+  end component;
+  component Counter_updn_n_bit_modif
+    generic ( n : NATURAL := 8 );
+    port(
+      rstb : IN STD_LOGIC;
+      clk : IN STD_LOGIC;
+      dir : IN STD_LOGIC;
+      bout : OUT STD_LOGIC_VECTOR((n - 1) downto 0)
+    );
+  end component;
+-----------------------------------
+begin
+------------------------------------
+-- Device under test (DUT)
+------------------------------------
+  CK: clk_1Hz
+  port map(
+    clk_50MHz => CLOCK_50,
+    rstb => SW(0),
+    clk_1Hz => CLK_SIG
+  );
+  DUT: Counter_updn_n_bit_modif
+  port map(
+    clk => CLK_SIG,
+    rstb => SW(0),
+    dir => SW(1),
+    bout => LEDR(7 downto 0)
+  );
+end architecture;
